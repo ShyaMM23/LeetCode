ed7820e6ad6325e7e8ba48847938f4bdd4ddafe1
ShyaMM23 <10727deepak@gmail.com>
2026-02-21 21:47:59 +0530

    Create searchrotated.java

---
+++ searchrotated.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i=0,j=nums.length-1;
+        while(i<=j){
+            int m=(i+j)/2;
+            if(nums[m]==target) return m;
+            if(nums[m]>=nums[i]){
+                if(target>=nums[i]&&target<nums[m]){
+                    j=m-1;
+                }
+                else{
+                    i=m+1;
+                }
+            }
+            else{
+                if(target>nums[m]&&target<=nums[j]){
+                    i=m+1;
+                }
+                else{
+                    j=m-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
