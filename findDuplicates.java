5a26ecb5603f797742738faa621d78bf79c01542
ShyaMM23 <10727deepak@gmail.com>
2026-02-21 21:54:10 +0530

    Create findDuplicates.java

---
+++ findDuplicates.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 1;i<nums.length;i++ )
+        {
+            if(nums[i]==nums[i-1]) 
+            {
+                res.add(nums[i]);
+            }
+        }
+        return res;
+    }
+}
