24c131a616e27fce227b8917464505a4331029b7
ShyaMM23 <10727deepak@gmail.com>
2026-02-21 22:38:13 +0530

    Create 1603.java

---
+++ 1603.java
@@ -0,0 +1,23 @@
+class ParkingSystem {
+
+    int arr[]=new int[3];
+    public ParkingSystem(int big, int medium, int small) {
+        this.arr[0]=big;
+        this.arr[1]=medium;
+        this.arr[2]=small;
+    }
+    
+    public boolean addCar(int n) {
+        if(arr[n-1]>0){arr[n-1]--;return true;}
+        else{
+            return false;
+        }
+    }
+    
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
