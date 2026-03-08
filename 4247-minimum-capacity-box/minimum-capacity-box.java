16c41df2c3985bd682a5ded410c5544bbd70994a
ShyaMM23 <10727deepak@gmail.com>
2026-03-08 13:01:52 +0530

    Time: 1 ms (23.29%) | Memory: 44.6 MB (57.97%) - LeetSync

---
+++ 4247-minimum-capacity-box/minimum-capacity-box.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumIndex(int[] capacity, int itemSize) {
+        int min=99999,r=-1;
+        for(int i=0;i<capacity.length;i++){
+            if(capacity[i]>= itemSize&&min>capacity[i])
+           { 
+                    min=capacity[i];
+                    r=i;
+            }
+        }
+        return r;
+    }
+}
